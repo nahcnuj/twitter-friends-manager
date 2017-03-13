8dbb4aaffc88dbf3ff7fbcfabf239a831953e25c
nahcnuj <junchan.kcct@gmail.com>
2017-03-14 03:30:18 +0900

    test

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const //http = require('http'),
+    fs = require('fs'),
+    ejs = require('ejs'),
+    //qs = require('qs'),
+    express = require('express');
+const settings = require('./settings');
+const port = process.env.port || settings.port;
+const template = fs.readFileSync(`${__dirname}/public_html/index.ejs`, 'utf-8');
+
+const app = express();
+app.listen(port);
+
+console.log(`Server running at port ${port}`);
+
+app.get('/', function(request, result) {
+    var data = ejs.render(template, {});
+    result.writeHead(200, {'Content-Type': 'text/html'});
+    result.write(data);
+    result.write(process.env.WEBSITE_NODE_DEFAULT_VERSION);
+    result.end();
+});
