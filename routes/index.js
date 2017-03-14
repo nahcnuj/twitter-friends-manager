9b6f461fb26a963d315aec0d2051a04794b7856a
nahcnuj <junchan.kcct@gmail.com>
2017-03-15 01:11:37 +0900

    followingのリストを取得するコードを実装

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,26 @@
-var twitter = require('twitter');
+var Twitter = require('twitter');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (req.session.oauth) {
-    res.render('index', { isLogined: true });
+    var client = new Twitter({
+      consumer_key: process.env.CONSUMER_KEY,
+      consumer_secret: process.env.CONSUMER_SECRET,
+      access_token_key: req.session.oauth.access_token,
+      access_token_secret: req.session.oauth.access_token_secret
+    });
+    console.log(client);
+    client.get('friends/list', {}, function(err, friends, response) {
+      if (err) {
+        next(err);
+      }
+      console.log(friends);
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(friends));
+    });
+    //res.render('index', { isLogined: true, twitter: req.session.twitter });
   }
   else {
     res.render('index', { isLogined: false });
